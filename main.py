628d0a4113652d0b818bf809be935be310caf13e
Patryk <zakos123456@gmail.com>
2023-06-16 23:10:19 +0200

    var change

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ driver.get("https://steamcommunity.com/login/home/?goto=")
 
 time.sleep(15)
 
-driver.get("https://steamcommunity.com/id/zak132465/edit/info")
+user = "zak132465"
+
+driver.get(f"https://steamcommunity.com/id/{user}/edit/info")
 name_box = driver.find_element(By.XPATH, "/html/body/div[1]/div[7]/div[4]/div/div[2]/div/div/div[3]/div[3]/div[2]/form/div[3]/div[2]/div[1]/label/div[2]/input")
 save_button = driver.find_element(By.XPATH, "/html/body/div[1]/div[7]/div[4]/div/div[2]/div/div/div[3]/div[3]/div[2]/form/div[7]/button[1]")
 
